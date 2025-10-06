cb4f36417c150686e8cf8b884966e9d5f5351d4f
Serge KOUAME <serge.kouame@groupemediasoft.com>
2025-10-06 13:40:34 +0000

    mise à jour des operations #2

--- src/routes/operation_routes.js
+++ src/routes/operation_routes.js
@@ -4,12 +4,13 @@ const app_auth = require('../middlewares/app_auth');
 const session_verify = require('../middlewares/session_verify');
 const operationController = require('../controllers/operation_controller');
 
+router.get('/', session_verify, operationController.getAllActeurOperations);
+
 router.post('/depot', session_verify, operationController.opDepot);
 router.post('/souscription', session_verify, operationController.opSouscription);
 router.get('/rachat', session_verify, operationController.opRachat);
 router.post('/transfert', session_verify, operationController.opTransfert);
 
-router.get('/', session_verify, operationController.getAllActeurOperations);
 router.get('/export/:debut/:fin/pdf', session_verify, operationController.exportActeurOperation);
 
 module.exports = router;
