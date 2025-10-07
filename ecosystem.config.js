6f034b49c4da1d90eafdf471a2a6dc33aeb4ab72
Serge KOUAME <serge.kouame@groupemediasoft.com>
2025-10-07 14:11:10 +0000

    pm2 deploiement config #7

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,22 +8,22 @@ module.exports = {
         max_memory_restart: "512M",
         env: {
             NODE_ENV: "development",
-            PORT: 3002
+            PORT: process.env.PORT || 3002
         },
         env_production: {
             NODE_ENV: "production",
-            PORT: 3002
+            PORT: process.env.PORT || 3002
         },
         log_date_format: "YYYY-MM-DD HH:mm Z"
     }],
 
     deploy: {
         production: {
-        user: "root",
-        host: "192.168.20.38",
-        ref: "origin/main",
-        repo: "https://github.com/kmsegeo/bnigestion-client-api.git",
-        path: "/home/mybank/bnigestion/bnigestion-client-api",
+        user: process.env.SERVER_USER,
+        host: process.env.SERVER_HOST,
+        ref: process.env.GIT_BRANCH,
+        repo: process.env.GIT_REPOSITORY,
+        path: process.env.SERVER_PATH,
         "pre-deploy-local": "",
         "post-deploy": "npm install && npm run build && pm2 reload ecosystem.config.js --env production",
         "pre-setup": ""
